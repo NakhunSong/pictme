ba308621d0ad84bf159e01442002a60492ef44c0
NakhunSong <nak4627@gmail.com>
2019-08-28 12:14:43 +0900

    Fix url

--- frontend/config/config.js
+++ frontend/config/config.js
@@ -1,3 +1,3 @@
-const backUrl = process.env.NODE_ENV === 'production' ? '//http://api.pictme.site' : 'http://localhost:3030';
+const backUrl = process.env.NODE_ENV === 'production' ? 'http://api.pictme.site' : 'http://localhost:3030';
 
 export { backUrl };
