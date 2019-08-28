4520c5e2c985ed3eb5ae9234de7bbbacf44d3a27
NakhunSong <nak4627@gmail.com>
2019-08-28 21:24:28 +0900

    Add resizing img Lambda

---
+++ lambda/index.js
@@ -0,0 +1,52 @@
+const AWS = require('aws-sdk');
+const Sharp = require('sharp');
+
+const S3 = new AWS.S3({ region: 'ap-northeast-2' });
+
+const transforms = [
+  { name: 'thumbnail_small', size: 320 },
+  { name: 'thumbnail_big', size: 640 },
+];
+
+exports.handler = async (event, context, callback) => {
+  const Bucket = event.Record[0].s3.bucket.name; // pictme
+  const Key = event.Record[0].s3.object.key; // 파일 경로(파일명 포함)
+  console.log(event);
+  console.log(event.Record[0]);
+  console.log(event.Record[1]);
+  const filename = Key.split('/')[Key.split('/').length - 1]; // ['directoryname', 'filename']
+  const ext = Key.split('.')[Key.split('.').length - 1]; // jpg, png, ...
+  console.log(Bucket, Key, filename, ext);
+  const requireFormat = ext === 'jpg' ? 'jpeg' : ext;
+
+  try {
+    const s3Object = await S3.getObject({
+      Bucket,
+      Key,
+    }).promise();
+    console.log('original', s3Object.Body.length);
+
+    // resizing & s3 put object
+    await Promise.all(
+      transforms.map(async item => {
+        const resizedImage = await Sharp(s3Object.body)
+          .resize(size, size, {
+            fit: 'inside',
+          })
+          .toFormat(requireFormat)
+          .toBuffer();
+        return await S3.putObject({
+          Bucket,
+          Key: `${item.name}/${filename}`,
+          Body: resizedImage,
+        }).promise();
+      })
+    );
+    console.log('put');
+
+    return callback(null, `Success: ${filename}`);
+  } catch (e) {
+    console.error(e);
+    return callback(e);
+  }
+};
